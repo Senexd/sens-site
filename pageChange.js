1d81f489e6e7d5899732aaa18e0e573338655af1
Sen <137726898+Senexd@users.noreply.github.com>
2025-02-27 13:57:54 -0500

    Construction page parity with main page; Construction page removed from index; comms still incomplete

--- pageChange.js
+++ pageChange.js
@@ -18,10 +18,10 @@ async function fadeSwap(currentDiv, newDiv) {
     if (!isAnimating && (currentDiv != newDiv)) {
         isAnimating = true;
         document.getElementById(currentDiv).style.animation = `fadeOut ${time}s linear`;
-        await new Promise(resolve => setTimeout(resolve, time * 1000));
+        await new Promise(resolve => setTimeout(resolve, time * 900));
         document.getElementById(currentDiv).style.display = "none";
         document.getElementById(newDiv).style.animation = `fadeIn ${time}s linear`;
-        await new Promise(resolve => setTimeout(resolve, time * 1000));
+        await new Promise(resolve => setTimeout(resolve, time * 900));
         document.getElementById(newDiv).style.display = "block";
         isAnimating = false;
         activeDiv = newDiv;
